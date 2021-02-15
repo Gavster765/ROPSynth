2f70c456b0194403bfa5c1af0eefdd99f276f936
Gavster765 <gwilko7@hotmail.com>
2021-02-15 20:40:46 +0000

    Use struct for vars

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include "gadgets.h"
 
+typedef struct Var {
+    char* name;
+    int value;
+} Var;
+
 int main(){
     // Gadgets gadgets = loadGadgets();
     int progLines = 3;
@@ -11,7 +16,7 @@ int main(){
         "Var y, 2",
         "Add x, y"
     };
-    char* vars[progLines];
+    Var vars[progLines];
 
     for (int i = 0 ; i < progLines ; i++){
         char* line = strdup(prog[i]);
@@ -30,9 +35,14 @@ int main(){
         }
         
         if(strcmp(opcode,"Var") == 0){
-            vars[i] = operandList[1];  // Use hastable?
+            Var newVar = {
+                operandList[0],
+                atoi(operandList[1])
+            };
+            vars[i] = newVar;
+              // Use hastable? Also probs shouldn't save value yet??
         }
     }
-    printf("%s %s\n",vars[0],vars[1]);
+    printf("%d %d\n",vars[0].value,vars[1].value);
     return 0;
 }
